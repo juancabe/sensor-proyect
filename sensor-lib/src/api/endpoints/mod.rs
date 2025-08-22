3dd31673a74a6977b69d157574974d422812a113
Juan Calzada <calzadaj@hotmail.es>
2025-08-23 01:51:01 +0200

    Remove unused sensor-lib now that endpoints have been migrated to server

--- sensor-lib/src/api/endpoints/mod.rs
+++
@@ -1,8 +0,0 @@
-pub mod get_sensor_data;
-pub mod login;
-pub mod post_sensor;
-pub mod post_sensor_data;
-pub mod post_user_summary;
-pub mod register;
-
-pub mod post_place;
--- sensor-lib/src/api/types/mod.rs
+++
@@ -1,1 +0,0 @@
-pub type Timestamp = i64;
